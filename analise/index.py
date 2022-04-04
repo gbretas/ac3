b6a0f8df722b1b876d659afc1027ec13c04d5755
Gustavo Bretas <gustavobretas5@gmail.com>
2022-04-04 10:23:02 -0300

    git

--- analise/index.py
+++ analise/index.py
@@ -1,6 +1,25 @@
 import os
 import sys
 
+#esquema de analise final
+
+# analise/final/trace-XY/Z-V.txt
+# X é a trace de comparacao 1
+# Y é a trace de comparacao 2
+# Z é a arquitetura 1
+# V é a arquitetura 2
+
+# Ou seja, na pasta trace-11 eu so comparo execuções da trace 1 vs execuções da trace 1
+
+# Em um arquivo 1-2.txt eu comparo a arquitetura 1 com a arquitetura 2
+
+# Pasta de resultados brutos:
+# res_brutos/XY.txt
+# X é a arquitetura
+# Y é a trace
+
+
+
 # 1.1 -> WriteBack e LRU - Trace 1
 # 1.2 -> WriteBack e LRU - Trace 2
 # 2.1 -> WriteBack e FIFO - Trace 1
